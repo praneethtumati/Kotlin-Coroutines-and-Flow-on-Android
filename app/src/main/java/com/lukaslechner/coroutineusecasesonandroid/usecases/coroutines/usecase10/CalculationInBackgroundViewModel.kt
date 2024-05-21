f29e3c71195ea5a1678ee5b4c723495779ebe862
Praneeth <Tumati.Praneeth@smartenergywater.in>
2024-05-21 15:46:12 +0530

    initial commit for understanding dispatchers.

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,33 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.launch
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            var result = BigInteger.ONE
+            val calculationTime = measureTimeMillis {
+                result = factorialOf(factorialOf)
+            }
+            var resultString = ""
+            val conversionTime = measureTimeMillis {
+                resultString = result.toString()
+            }
+            uiState.value = UiState.Success(resultString,calculationTime,conversionTime)
+        }
+    }
 
+    private fun factorialOf(number:Int):BigInteger{
+        var result = BigInteger.ONE
+        for(i in 1..number){
+            result = result.multiply(BigInteger.valueOf(i.toLong()))
+        }
+        return result
     }
 }
